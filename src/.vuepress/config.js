db7c4e261912df0d11b77be77ccd4c9e0613b42b
zhoubichuan <zhoubichuan@icloud.com>
2021-06-04 22:41:12 +0800

    master

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -60,11 +60,11 @@ module.exports = {
         nav: [{
             text: '基础知识',
             items: [{
-                text: '1.React前端工程化',
+                text: '1.前端工程化',
                 link: '/base/build/1.config'
               },
               {
-                text: '2.React基础知识',
+                text: '2.基础知识',
                 link: '/base/practice/base',
               }
             ]
