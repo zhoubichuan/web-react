23d3700aaf2ad9abcec6afb9c3ba76a3b96f6ebd
zhoubichuan <zhoubichuan@icloud.com>
2021-08-31 22:47:25 +0800

    master

--- gitee.sh
+++ gitee.sh
@@ -4,11 +4,11 @@ npm run src:build
 # 3.进入打包目录
 cd dist
 
+git remote add gitee https://gitee.com/zhoubichuan/web-react.git
 git init
 git checkout --orphan gh-pages
 git add .
 git commit -m 'gh-pages'
-git remote add gitee https://gitee.com/zhoubichuan/web-react.git
 git push -f gitee gh-pages
 
 cd -
