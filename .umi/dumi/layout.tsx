ca948b17c43ee315fe4581a6fbcd8baf0a38c44a
zhoubichuan <zhoubichuan@icloud.com>
2022-04-04 03:54:40 +0800

    使用dumi

--- .umi/dumi/layout.tsx
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import React from 'react';
-import config from '@@/dumi/config';
-import demos from '@@/dumi/demos';
-import apis from '@@/dumi/apis';
-import Layout from '/Users/zhoubichuan/Web-React/node_modules/_@umijs_preset-dumi@1.1.40@@umijs/preset-dumi/lib/theme/layout';
-
-export default (props) => <Layout {...props} config={config} demos={demos} apis={apis} />;
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 # 1.发布到master分支
 git add -A
 git commit -m 'master'
+git remote add gitee https://gitee.com/zhoubichuan/web-react.git
 git pull gitee master
 git push gitee master
 # 2.打包
@@ -19,27 +20,24 @@ git push -f gitee gh-pages
 
 cd -
 
-set -e
-shopt -s extglob
-
-TEMP_PATH="docs/.temp"
-
-# build docs
-npm run build
-
-# prepare deploy
-rm -rf $TEMP_PATH
-mkdir $TEMP_PATH
-cd $TEMP_PATH
-git init
-git pull https://github.com/zhoubichuan/web-react.git gh-pages
-cp -r ../../dist/* .
-
-# commit and push changes
+# 1.发布到master分支
 git add -A
-git commit --am -m "build: deploy documentation"
-git push -f https://github.com/zhoubichuan/web-react.git master:gh-pages
+git commit -m 'master'
+git remote add github https://github.com/zhoubichuan/web-react.git
+git pull github master
+git push github master
+# 2.打包
+npm run src:build
 
-## clean
-cd
-rm -rf $TEMP_PATH
+# 3.进入打包目录
+cd dist
+
+# 4.发布到gh-pages分支
+git init
+git checkout --orphan gh-pages
+git add .
+git commit -m 'gh-pages'
+git remote add github https://github.com/zhoubichuan/web-react.git
+git push -f github gh-pages
+
+cd -
